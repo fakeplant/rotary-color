7f06844f00b945f153c61427e01ebb286dbfd1e8
plant <fake@plant.dev>
2024-05-27 17:54:33 -0600

    Revert "Test pattern"
    This reverts commit 02b72b7b91e77e3ac547095f237d58106e40043f.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 #include <Wire.h>
-#include <lvgl.h>
 
 /*******************************************************************************
  * Setup RGB Display
@@ -28,182 +27,601 @@ Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
     st7701_type5_init_operations, sizeof(st7701_type5_init_operations));
 
 /*******************************************************************************
- * Program
+ * Setup RGB Display
  ******************************************************************************/
 
-uint32_t screenWidth;
-uint32_t screenHeight;
-uint32_t bufSize;
-lv_display_t *disp;
-lv_color_t *disp_draw_buf;
-
-#if LV_USE_LOG != 0
-void my_print(lv_log_level_t level, const char *buf) {
-  LV_UNUSED(level);
-  Serial.println(buf);
-  Serial.flush();
-}
+#ifdef ESP32
+#undef F
+#define F(s) (s)
 #endif
 
-uint32_t millis_cb(void) { return millis(); }
+int32_t w, h, n, n1, cx, cy, cx1, cy1, cn, cn1;
+uint8_t tsa, tsb, tsc, ds;
 
-/* LVGL calls it when a rendered image needs to copied to the display*/
-void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
-#ifndef DIRECT_MODE
-  uint32_t w = lv_area_get_width(area);
-  uint32_t h = lv_area_get_height(area);
-
-  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)px_map, w, h);
-#endif // #ifndef DIRECT_MODE
-
-  /*Call it to tell LVGL you are ready*/
-  lv_disp_flush_ready(disp);
-}
-
-/*Read the touchpad*/
-void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data) {
-  if (touch_has_signal()) {
-    if (touch_touched()) {
-      data->state = LV_INDEV_STATE_PRESSED;
-
-      /*Set the coordinates*/
-      data->point.x = touch_last_x;
-      data->point.y = touch_last_y;
-    } else if (touch_released()) {
-      data->state = LV_INDEV_STATE_RELEASED;
-    }
-  } else {
-    data->state = LV_INDEV_STATE_RELEASED;
-  }
-}
+// Forward declarations of functions used in the sketch
+static inline uint32_t micros_start();
+void serialOut(const char *item, int32_t v, uint32_t d, bool clear);
+void printnice(const char *item, long int v);
+int32_t testFillScreen();
+int32_t testText();
+int32_t testPixels();
+int32_t testLines();
+int32_t testFastLines();
+int32_t testFilledRects();
+int32_t testRects();
+int32_t testFilledTriangles();
+int32_t testTriangles();
+int32_t testFilledCircles(uint8_t radius);
+int32_t testCircles(uint8_t radius);
+int32_t testFillArcs();
+int32_t testArcs();
+int32_t testFilledRoundRects();
+int32_t testRoundRects();
 
 void setup() {
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
   // while(!Serial);
-  Serial.println("Arduino_GFX LVGL_Arduino example v9");
-
-  String LVGL_Arduino = "Hello Arduino! ";
-  LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() +
-                  "." + lv_version_patch();
-
-  Serial.println(LVGL_Arduino);
+  Serial.println("Arduino_GFX PDQgraphicstest example!");
 
 #ifdef GFX_EXTRA_PRE_INIT
   GFX_EXTRA_PRE_INIT();
 #endif
 
   // Init Display
-  if (!gfx->begin()) {
+  if (!gfx->begin())
+  // if (!gfx->begin(80000000)) /* specify data bus speed */
+  {
     Serial.println("gfx->begin() failed!");
   }
-  gfx->fillScreen(BLACK);
+
+  w = gfx->width();
+  h = gfx->height();
+  n = min(w, h);
+  n1 = n - 1;
+  cx = w / 2;
+  cy = h / 2;
+  cx1 = cx - 1;
+  cy1 = cy - 1;
+  cn = min(cx1, cy1);
+  cn1 = cn - 1;
+  tsa = ((w <= 176) || (h <= 160))
+            ? 1
+            : (((w <= 240) || (h <= 240)) ? 2 : 3); // text size A
+  tsb = ((w <= 272) || (h <= 220)) ? 1 : 2;         // text size B
+  tsc = ((w <= 220) || (h <= 220)) ? 1 : 2;         // text size C
+  ds = (w <= 160) ? 9 : 12;                         // digit size
 
 #ifdef GFX_BL
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, HIGH);
 #endif
-
-  // Init touch device
-  touch_init(gfx->width(), gfx->height(), gfx->getRotation());
-
-  lv_init();
-
-  /*Set a tick source so that LVGL will know how much time elapsed. */
-  lv_tick_set_cb(millis_cb);
-
-  /* register print function for debugging */
-#if LV_USE_LOG != 0
-  lv_log_register_print_cb(my_print);
-#endif
-
-  screenWidth = gfx->width();
-  screenHeight = gfx->height();
-
-#ifdef DIRECT_MODE
-  bufSize = screenWidth * screenHeight;
-#else
-  bufSize = screenWidth * 40;
-#endif
-
-#ifdef ESP32
-#if defined(DIRECT_MODE) && defined(RGB_PANEL)
-  disp_draw_buf = (lv_color_t *)gfx->getFramebuffer();
-#else  // !DIRECT_MODE
-  disp_draw_buf = (lv_color_t *)heap_caps_malloc(
-      bufSize * 2, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-  if (!disp_draw_buf) {
-    // remove MALLOC_CAP_INTERNAL flag try again
-    disp_draw_buf =
-        (lv_color_t *)heap_caps_malloc(bufSize * 2, MALLOC_CAP_8BIT);
-  }
-#endif // !DIRECT_MODE
-#else  // !ESP32
-  Serial.println(
-      "LVGL draw_buf allocate MALLOC_CAP_INTERNAL failed! malloc again...");
-  disp_draw_buf = (lv_color_t *)malloc(bufSize * 2);
-#endif // !ESP32
-  if (!disp_draw_buf) {
-    Serial.println("LVGL disp_draw_buf allocate failed!");
-  } else {
-    disp = lv_display_create(screenWidth, screenHeight);
-    lv_display_set_flush_cb(disp, my_disp_flush);
-#ifdef DIRECT_MODE
-    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2,
-                           LV_DISPLAY_RENDER_MODE_DIRECT);
-#else
-    lv_display_set_buffers(disp, disp_draw_buf, NULL, bufSize * 2,
-                           LV_DISPLAY_RENDER_MODE_PARTIAL);
-#endif
-
-    /*Initialize the (dummy) input device driver*/
-    lv_indev_t *indev = lv_indev_create();
-    lv_indev_set_type(
-        indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
-    lv_indev_set_read_cb(indev, my_touchpad_read);
-
-    /* Option 1: Create a simple label
-     * ---------------------
-     */
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(
-        label,
-        "Hello Arduino, I'm LVGL!(V" GFX_STR(LVGL_VERSION_MAJOR) "." GFX_STR(
-            LVGL_VERSION_MINOR) "." GFX_STR(LVGL_VERSION_PATCH) ")");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-
-    /* Option 2: Try an example. See all the examples
-     *  - Online: https://docs.lvgl.io/master/examples.html
-     *  - Source codes: https://github.com/lvgl/lvgl/tree/master/examples
-     * ----------------------------------------------------------------
-     */
-    // lv_example_btn_1();
-
-    /* Option 3: Or try out a demo. Don't forget to enable the demos in
-     * lv_conf.h. E.g. LV_USE_DEMOS_WIDGETS
-     * -------------------------------------------------------------------------------------------
-     */
-    // lv_demo_widgets();
-    // lv_demo_benchmark();
-    // lv_demo_keypad_encoder();
-    // lv_demo_music();
-    // lv_demo_stress();
-  }
-
-  Serial.println("Setup done");
 }
 
-void loop() {
-  lv_task_handler(); /* let the GUI do its work */
+static inline uint32_t micros_start() __attribute__((always_inline));
+static inline uint32_t micros_start() {
+  uint8_t oms = millis();
+  while ((uint8_t)millis() == oms)
+    ;
+  return micros();
+}
 
-#ifdef DIRECT_MODE
-#ifdef RGB_PANEL
+void loop(void) {
+  Serial.println(F("Benchmark\tmicro-secs"));
+
+  int32_t usecFillScreen = testFillScreen();
+  serialOut(F("Screen fill\t"), usecFillScreen, 100, true);
+
+  int32_t usecText = testText();
+  serialOut(F("Text\t"), usecText, 3000, true);
+
+  int32_t usecPixels = testPixels();
+  serialOut(F("Pixels\t"), usecPixels, 100, true);
+
+  int32_t usecLines = testLines();
+  serialOut(F("Lines\t"), usecLines, 100, true);
+
+  int32_t usecFastLines = testFastLines();
+  serialOut(F("Horiz/Vert Lines\t"), usecFastLines, 100, true);
+
+  int32_t usecFilledRects = testFilledRects();
+  serialOut(F("Rectangles (filled)\t"), usecFilledRects, 100, false);
+
+  int32_t usecRects = testRects();
+  serialOut(F("Rectangles (outline)\t"), usecRects, 100, true);
+
+  int32_t usecFilledTrangles = testFilledTriangles();
+  serialOut(F("Triangles (filled)\t"), usecFilledTrangles, 100, false);
+
+  int32_t usecTriangles = testTriangles();
+  serialOut(F("Triangles (outline)\t"), usecTriangles, 100, true);
+
+  int32_t usecFilledCircles = testFilledCircles(10);
+  serialOut(F("Circles (filled)\t"), usecFilledCircles, 100, false);
+
+  int32_t usecCircles = testCircles(10);
+  serialOut(F("Circles (outline)\t"), usecCircles, 100, true);
+
+  int32_t usecFilledArcs = testFillArcs();
+  serialOut(F("Arcs (filled)\t"), usecFilledArcs, 100, false);
+
+  int32_t usecArcs = testArcs();
+  serialOut(F("Arcs (outline)\t"), usecArcs, 100, true);
+
+  int32_t usecFilledRoundRects = testFilledRoundRects();
+  serialOut(F("Rounded rects (filled)\t"), usecFilledRoundRects, 100, false);
+
+  int32_t usecRoundRects = testRoundRects();
+  serialOut(F("Rounded rects (outline)\t"), usecRoundRects, 100, true);
+
+#ifdef CANVAS
+  uint32_t start = micros_start();
   gfx->flush();
-#else
-  gfx->draw16bitRGBBitmap(0, 0, (uint16_t *)draw_buf, screenWidth,
-                          screenHeight);
+  int32_t usecFlush = micros() - start;
+  serialOut(F("flush (Canvas only)\t"), usecFlush, 0, false);
 #endif
-#endif // #ifdef DIRECT_MODE
 
-  delay(5);
+  Serial.println(F("Done!"));
+
+  uint16_t c = 4;
+  int8_t d = 1;
+  for (int32_t i = 0; i < h; i++) {
+    gfx->drawFastHLine(0, i, w, c);
+    c += d;
+    if (c <= 4 || c >= 11) {
+      d = -d;
+    }
+  }
+
+  gfx->setCursor(0, 0);
+
+  gfx->setTextSize(tsa);
+  gfx->setTextColor(MAGENTA);
+  gfx->println(F("Arduino GFX PDQ"));
+
+  if (h > w) {
+    gfx->setTextSize(tsb);
+    gfx->setTextColor(GREEN);
+    gfx->print(F("\nBenchmark "));
+    gfx->setTextSize(tsc);
+    if (ds == 12) {
+      gfx->print(F("   "));
+    }
+    gfx->println(F("micro-secs"));
+  }
+
+  printnice(F("Screen fill "), usecFillScreen);
+  printnice(F("Text        "), usecText);
+  printnice(F("Pixels      "), usecPixels);
+  printnice(F("Lines       "), usecLines);
+  printnice(F("H/V Lines   "), usecFastLines);
+  printnice(F("Rectangles F"), usecFilledRects);
+  printnice(F("Rectangles  "), usecRects);
+  printnice(F("Triangles F "), usecFilledTrangles);
+  printnice(F("Triangles   "), usecTriangles);
+  printnice(F("Circles F   "), usecFilledCircles);
+  printnice(F("Circles     "), usecCircles);
+  printnice(F("Arcs F      "), usecFilledArcs);
+  printnice(F("Arcs        "), usecArcs);
+  printnice(F("RoundRects F"), usecFilledRoundRects);
+  printnice(F("RoundRects  "), usecRoundRects);
+
+  if ((h > w) || (h > 240)) {
+    gfx->setTextSize(tsc);
+    gfx->setTextColor(GREEN);
+    gfx->print(F("\nBenchmark Complete!"));
+  }
+
+#ifdef CANVAS
+  gfx->flush();
+#endif
+
+  delay(60 * 1000L);
+}
+
+#ifdef ESP32
+void serialOut(const char *item, int32_t v, uint32_t d, bool clear)
+#else
+void serialOut(const __FlashStringHelper *item, int32_t v, uint32_t d,
+               bool clear)
+#endif
+{
+#ifdef CANVAS
+  gfx->flush();
+#endif
+  Serial.print(item);
+  if (v < 0) {
+    Serial.println(F("N/A"));
+  } else {
+    Serial.println(v);
+  }
+  delay(d);
+  if (clear) {
+    gfx->fillScreen(BLACK);
+  }
+}
+
+#ifdef ESP32
+void printnice(const char *item, long int v)
+#else
+void printnice(const __FlashStringHelper *item, long int v)
+#endif
+{
+  gfx->setTextSize(tsb);
+  gfx->setTextColor(CYAN);
+  gfx->print(item);
+
+  gfx->setTextSize(tsc);
+  gfx->setTextColor(YELLOW);
+  if (v < 0) {
+    gfx->println(F("      N / A"));
+  } else {
+    char str[32] = {0};
+#ifdef RTL8722DM
+    sprintf(str, "%d", (int)v);
+#else
+    sprintf(str, "%ld", v);
+#endif
+    for (char *p = (str + strlen(str)) - 3; p > str; p -= 3) {
+      memmove(p + 1, p, strlen(p) + 1);
+      *p = ',';
+    }
+    while (strlen(str) < ds) {
+      memmove(str + 1, str, strlen(str) + 1);
+      *str = ' ';
+    }
+    gfx->println(str);
+  }
+}
+
+int32_t testFillScreen() {
+  uint32_t start = micros_start();
+  // Shortened this tedious test!
+  gfx->fillScreen(WHITE);
+  gfx->fillScreen(RED);
+  gfx->fillScreen(GREEN);
+  gfx->fillScreen(BLUE);
+  gfx->fillScreen(BLACK);
+
+  return micros() - start;
+}
+
+int32_t testText() {
+  uint32_t start = micros_start();
+  gfx->setCursor(0, 0);
+
+  gfx->setTextSize(1);
+  gfx->setTextColor(WHITE, BLACK);
+  gfx->println(F("Hello World!"));
+
+  gfx->setTextSize(2);
+  gfx->setTextColor(gfx->color565(0xff, 0x00, 0x00));
+  gfx->print(F("RED "));
+  gfx->setTextColor(gfx->color565(0x00, 0xff, 0x00));
+  gfx->print(F("GREEN "));
+  gfx->setTextColor(gfx->color565(0x00, 0x00, 0xff));
+  gfx->println(F("BLUE"));
+
+  gfx->setTextSize(tsa);
+  gfx->setTextColor(YELLOW);
+  gfx->println(1234.56);
+
+  gfx->setTextColor(WHITE);
+  gfx->println((w > 128) ? 0xDEADBEEF : 0xDEADBEE, HEX);
+
+  gfx->setTextColor(CYAN, WHITE);
+  gfx->println(F("Groop,"));
+
+  gfx->setTextSize(tsc);
+  gfx->setTextColor(MAGENTA, WHITE);
+  gfx->println(F("I implore thee,"));
+
+  gfx->setTextSize(1);
+  gfx->setTextColor(NAVY, WHITE);
+  gfx->println(F("my foonting turlingdromes."));
+
+  gfx->setTextColor(DARKGREEN, WHITE);
+  gfx->println(F("And hooptiously drangle me"));
+
+  gfx->setTextColor(DARKCYAN, WHITE);
+  gfx->println(F("with crinkly bindlewurdles,"));
+
+  gfx->setTextColor(MAROON, WHITE);
+  gfx->println(F("Or I will rend thee"));
+
+  gfx->setTextColor(PURPLE, WHITE);
+  gfx->println(F("in the gobberwartsb"));
+
+  gfx->setTextColor(OLIVE, WHITE);
+  gfx->println(F("with my blurglecruncheon,"));
+
+  gfx->setTextColor(DARKGREY, WHITE);
+  gfx->println(F("see if I don't!"));
+
+  gfx->setTextSize(2);
+  gfx->setTextColor(RED);
+  gfx->println(F("Size 2"));
+
+  gfx->setTextSize(3);
+  gfx->setTextColor(ORANGE);
+  gfx->println(F("Size 3"));
+
+  gfx->setTextSize(4);
+  gfx->setTextColor(YELLOW);
+  gfx->println(F("Size 4"));
+
+  gfx->setTextSize(5);
+  gfx->setTextColor(GREENYELLOW);
+  gfx->println(F("Size 5"));
+
+  gfx->setTextSize(6);
+  gfx->setTextColor(GREEN);
+  gfx->println(F("Size 6"));
+
+  gfx->setTextSize(7);
+  gfx->setTextColor(BLUE);
+  gfx->println(F("Size 7"));
+
+  gfx->setTextSize(8);
+  gfx->setTextColor(PURPLE);
+  gfx->println(F("Size 8"));
+
+  gfx->setTextSize(9);
+  gfx->setTextColor(PALERED);
+  gfx->println(F("Size 9"));
+
+  return micros() - start;
+}
+
+int32_t testPixels() {
+  uint32_t start = micros_start();
+
+  for (int16_t y = 0; y < h; y++) {
+    for (int16_t x = 0; x < w; x++) {
+      gfx->drawPixel(x, y, gfx->color565(x << 3, y << 3, x * y));
+    }
+#ifdef ESP8266
+    yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+  }
+
+  return micros() - start;
+}
+
+int32_t testLines() {
+  uint32_t start;
+  int32_t x1, y1, x2, y2;
+
+  start = micros_start();
+
+  x1 = y1 = 0;
+  y2 = h - 1;
+  for (x2 = 0; x2 < w; x2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x2 = w - 1;
+  for (y2 = 0; y2 < h; y2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x1 = w - 1;
+  y1 = 0;
+  y2 = h - 1;
+  for (x2 = 0; x2 < w; x2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x2 = 0;
+  for (y2 = 0; y2 < h; y2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x1 = 0;
+  y1 = h - 1;
+  y2 = 0;
+  for (x2 = 0; x2 < w; x2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x2 = w - 1;
+  for (y2 = 0; y2 < h; y2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x1 = w - 1;
+  y1 = h - 1;
+  y2 = 0;
+  for (x2 = 0; x2 < w; x2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  x2 = 0;
+  for (y2 = 0; y2 < h; y2 += 6) {
+    gfx->drawLine(x1, y1, x2, y2, BLUE);
+  }
+#ifdef ESP8266
+  yield(); // avoid long run triggered ESP8266 WDT restart
+#endif
+
+  return micros() - start;
+}
+
+int32_t testFastLines() {
+  uint32_t start;
+  int32_t x, y;
+
+  start = micros_start();
+
+  for (y = 0; y < h; y += 5) {
+    gfx->drawFastHLine(0, y, w, RED);
+  }
+  for (x = 0; x < w; x += 5) {
+    gfx->drawFastVLine(x, 0, h, BLUE);
+  }
+
+  return micros() - start;
+}
+
+int32_t testFilledRects() {
+  uint32_t start;
+  int32_t i, i2;
+
+  start = micros_start();
+
+  for (i = n; i > 0; i -= 6) {
+    i2 = i / 2;
+
+    gfx->fillRect(cx - i2, cy - i2, i, i, gfx->color565(i, i, 0));
+  }
+
+  return micros() - start;
+}
+
+int32_t testRects() {
+  uint32_t start;
+  int32_t i, i2;
+
+  start = micros_start();
+  for (i = 2; i < n; i += 6) {
+    i2 = i / 2;
+    gfx->drawRect(cx - i2, cy - i2, i, i, GREEN);
+  }
+
+  return micros() - start;
+}
+
+int32_t testFilledCircles(uint8_t radius) {
+  uint32_t start;
+  int32_t x, y, r2 = radius * 2;
+
+  start = micros_start();
+
+  for (x = radius; x < w; x += r2) {
+    for (y = radius; y < h; y += r2) {
+      gfx->fillCircle(x, y, radius, MAGENTA);
+    }
+  }
+
+  return micros() - start;
+}
+
+int32_t testCircles(uint8_t radius) {
+  uint32_t start;
+  int32_t x, y, r2 = radius * 2;
+  int32_t w1 = w + radius;
+  int32_t h1 = h + radius;
+
+  // Screen is not cleared for this one -- this is
+  // intentional and does not affect the reported time.
+  start = micros_start();
+
+  for (x = 0; x < w1; x += r2) {
+    for (y = 0; y < h1; y += r2) {
+      gfx->drawCircle(x, y, radius, WHITE);
+    }
+  }
+
+  return micros() - start;
+}
+
+int32_t testFillArcs() {
+  int16_t i, r = 360 / cn;
+  uint32_t start = micros_start();
+
+  for (i = 6; i < cn; i += 6) {
+    gfx->fillArc(cx1, cy1, i, i - 3, 0, i * r, RED);
+  }
+
+  return micros() - start;
+}
+
+int32_t testArcs() {
+  int16_t i, r = 360 / cn;
+  uint32_t start = micros_start();
+
+  for (i = 6; i < cn; i += 6) {
+    gfx->drawArc(cx1, cy1, i, i - 3, 0, i * r, WHITE);
+  }
+
+  return micros() - start;
+}
+
+int32_t testFilledTriangles() {
+  uint32_t start;
+  int32_t i;
+
+  start = micros_start();
+
+  for (i = cn1; i > 10; i -= 5) {
+    gfx->fillTriangle(cx1, cy1 - i, cx1 - i, cy1 + i, cx1 + i, cy1 + i,
+                      gfx->color565(0, i, i));
+  }
+
+  return micros() - start;
+}
+
+int32_t testTriangles() {
+  uint32_t start;
+  int32_t i;
+
+  start = micros_start();
+
+  for (i = 0; i < cn; i += 5) {
+    gfx->drawTriangle(cx1, cy1 - i,     // peak
+                      cx1 - i, cy1 + i, // bottom left
+                      cx1 + i, cy1 + i, // bottom right
+                      gfx->color565(0, 0, i));
+  }
+
+  return micros() - start;
+}
+
+int32_t testFilledRoundRects() {
+  uint32_t start;
+  int32_t i, i2;
+
+  start = micros_start();
+
+  for (i = n1; i > 20; i -= 6) {
+    i2 = i / 2;
+    gfx->fillRoundRect(cx - i2, cy - i2, i, i, i / 8, gfx->color565(0, i, 0));
+  }
+
+  return micros() - start;
+}
+
+int32_t testRoundRects() {
+  uint32_t start;
+  int32_t i, i2;
+
+  start = micros_start();
+
+  for (i = 20; i < n1; i += 6) {
+    i2 = i / 2;
+    gfx->drawRoundRect(cx - i2, cy - i2, i, i, i / 8, gfx->color565(i, 0, 0));
+  }
+
+  return micros() - start;
 }
